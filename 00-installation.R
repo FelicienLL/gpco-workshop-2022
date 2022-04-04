f70201511cec5bcae6c14fd54fdbecd7885b6529
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2022-04-04 14:49:44 +0200

    expectation installation rtools

--- 00-installation.R
+++ 00-installation.R
@@ -7,6 +7,10 @@ install.packages("mapbayr")
 
 # Check if installation is succesful
 Sys.which("make")
+
+# You should obtain something like:
+# "C:\\rtools40\\usr\\bin\\make.exe"
+
 # Otherwise, for windows users :
 # go to: https://cran.r-project.org/bin/windows/Rtools/rtools40.html
 # write('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', file = "~/.Renviron", append = TRUE)
