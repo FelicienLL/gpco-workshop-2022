ba58bdacd233203e3a472f3c9851e229110a8b95
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2022-11-24 17:48:52 +0100

    new "rappel" file

---
+++ 01-rappels.R
@@ -0,0 +1,38 @@
+#' R : Rappels de Base.
+
+#' Assigner
+1 + 1
+a <- 1 + 1
+a
+a + 1
+a
+a <- a + 1
+a
+
+#' Fonctions
+exp(x = 1)
+exp(a)
+rnorm(n = 10, mean = 0, sd = 1)
+rnorm(10, 0, 1)
+
+#' Package
+#' 1. INSTALLER
+#' install.packages("mapbayr")
+(.packages())
+#' 2. CHARGER
+library(mapbayr)
+(.packages())
+
+library(tidyverse)
+(.packages())
+#' Pipe
+log(10)
+#' CTRL + SHIFT + M
+10 %>%
+  log()
+
+exp(sqrt(log(10)))
+10 %>%
+  log() %>%
+  sqrt() %>%
+  exp()
