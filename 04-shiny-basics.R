ef78956bcb1ab1c4bd430b33a1a789c49c5b77ad
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2022-11-28 17:19:23 +0100

    simpler basic shiny code

--- 04-shiny-basics.R
+++ 04-shiny-basics.R
@@ -3,19 +3,16 @@ ui <- fluidPage(
     titlePanel("La loi Normale"),
     sidebarLayout(
         sidebarPanel(
-            sliderInput(inputId = "nsample",
-                        label = "Nombre d'echantillons",
-                        min = 10,
-                        max = 1000,
-                        value = 30)
+            sliderInput(inputId = "nsample", label = "Nombre d'echantillons",
+                        min = 10, max = 1000, value = 30)
         ),
         mainPanel(
-           plotOutput("distributionPlot")
+           plotOutput("distributionplot")
         )
     )
 )
 server <- function(input, output) {
-  output$distributionPlot <- renderPlot({
+  output$distributionplot <- renderPlot({
     values <- rnorm(n = input$nsample, mean = 0, sd = 1)
     hist(x = values, main = "Distribution normale")
   })
