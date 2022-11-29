876a06453e404c8dd5ecf44b9eb3410688eb947a
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2022-11-29 12:07:06 +0100

    shiny folder

---
+++ 04-shiny-basics/brouillon.R
@@ -0,0 +1,10 @@
+# App1 : Basic App
+hist(rnorm(n = 90), main = "Distribution normale", col = "grey")
+
+# App2 : A partir de App1, ajouter un input pour changer la couleur
+
+
+# App3 : A partir de App2, ajouter un output pour calculer la moyenne reelle
+
+
+# App4 : A partir de App3, ajouter un bouton "Go!"
